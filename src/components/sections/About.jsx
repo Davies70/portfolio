0c6d8cc684e73e11660e1ae43c23ee826a1a30ec
ajayi daves <ajayid89@gmail.com>
2024-12-07 01:01:29 +0100

    Improve readability of About section by restructuring web app description for CheapBites

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -158,14 +158,13 @@ const About = () => {
 
             <p>
               I also recently made{' '}
-              <a href='https://cheapbites.vercel.app'>a web app</a>
-              designed to help food lovers find budget-friendly dining options
-              nearby. It features interactive maps, personalized
-              recommendations, and user-generated reviews, powered by APIs like
-              FourSquare and Geoapify. Built with Next.js for the backend and
-              MongoDB for data management, CheapBites allows users to manage
-              visited locations, save favorites, and enjoy a tailored dining
-              experience.
+              <a href='https://cheapbites.vercel.app'>a web app</a> designed to
+              help food lovers find budget-friendly dining options nearby. It
+              features interactive maps, personalized recommendations, and
+              user-generated reviews, powered by APIs like FourSquare and
+              Geoapify. Built with Next.js for the backend and MongoDB for data
+              management, CheapBites allows users to manage visited locations,
+              save favorites, and enjoy a tailored dining experience.
             </p>
 
             <p>A few technologies I’ve been working with recently:</p>
